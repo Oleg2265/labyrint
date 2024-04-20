9af6db61f40d913cb82764de1c1836cfc309b304
copibra20234567 <kulycky@gmail.com>
2024-04-20 19:29:58 +0300

    lesson5

--- Character.py
+++ Character.py
@@ -9,23 +9,31 @@ class Player:
         self.hitbox.x = x
         self.hitbox.y = y
         self.speed = speed
+        self.w = w
+        self.h = h
 
 
     def draw(self, window):
         #pygame.draw.rect(window, (0,0,255), self.hitbox)
         window.blit(self.photo, (self.hitbox.x, self.hitbox.y))
+    def change_photo(self, photo):
+        self.photo = pygame.transform.scale(pygame.image.load(photo), (self.w, self.h))
+
 
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             self.hitbox.y -= self.speed
+            self.change_photo("img.png")
         if keys[pygame.K_s]:
             self.hitbox.y += self.speed
+            self.change_photo("img_5.png")
         if keys[pygame.K_a]:
             self.hitbox.x -= self.speed
+            self.change_photo("img_4.png")
         if keys[pygame.K_d]:
             self.hitbox.x += self.speed
-
+            self.change_photo("img.png")
 
 class Bad:
     def __init__(self, x, y, w, h, img, speed, ex, ey, speedx, speedy):
@@ -76,6 +84,24 @@ class object:
     def __init__(self,x, y, w, h, img,):
         self.photo = pygame.transform.scale(pygame.image.load(img), (w, h))
         self.hitbox = self.photo.get_rect()
+        self.w = w
+        self.h = h
+        self.hitbox.x = x
+        self.hitbox.y = y
+
+    def draw(self, window):
+        #pygame.draw.rect(window, (0,0,255), self.hitbox)
+        window.blit(self.photo, (self.hitbox.x, self.hitbox.y))
+
+    def change_photo(self, photo):
+        self.photo = pygame.transform.scale(pygame.image.load(photo), (self.w, self.h))
+
+
+class wall:
+    def __init__(self,x, y, w, h, img,):
+        self.photo = pygame.transform.scale(pygame.image.load(img), (w, h))
+        self.hitbox = self.photo.get_rect()
+
         self.hitbox.x = x
         self.hitbox.y = y
 
--- Main.py
+++ Main.py
@@ -1,53 +1,90 @@
 import pygame
 from pygame import image
+import pygame
 from Character import *
 window = pygame.display.set_mode((700, 500))
 fps = pygame.time.Clock()
 
+pygame.init()
+pygame.mixer.init()
+sound = pygame.mixer.Sound("Sound_kaka.mp3")
 
-background = pygame.transform.scale(image.load("background.png"), (700, 500))
+pygame.mixer.music.load("Sound_play.mp3")
+pygame.mixer.music.play(-1)
 
-roma = Player(350 , 250, 80, 80, "img.png", 4)
-worog = Bad(400, 100, 65, 65, "img_1.png", 4, 90, 90, 4, 4)
-worog1 = Bad(400, 400, 65, 65, "img_1.png", 4, 90, 90, 4, 4)
-worog2 = Bad(100, 400, 65, 65, "img_1.png", 4, 90, 90, 4, 4)
-worog3 = Bad(300, 200, 65, 65, "img_1.png", 4, 90, 90, 4, 4)
-worog4 = Bad(100, 300, 65, 65, "img_1.png", 4, 90, 90, 4, 4)
-apple1 = object(10, 10, 50, 50, "img_2.png")
-apple2 = object(690 , 10, 50, 50, "img_2.png")
-apple3 = object(10, 490, 50, 50, "img_2.png")
-apple4 = object(690, 490, 50, 50, "img_2.png")
+back_theme = 0
 
 
+
+
+
+
+
+
+
+roma = Player(320 , 260, 80, 80, "img.png", 4)
+worog = Bad(400, 100, 35, 35, "img_1.png", 4, 25, 10, 4, 4)
+worog1 = Bad(400, 200, 35, 35, "img_1.png", 4, 25, 180, 4, 4)
+
+
+apple1 = object(200, 30, 50, 50, "img_2.png")
+lever = object(20, 420, 60, 60, "Right_lever.png")
+back = object(0, 0, 700, 500, "Space_background.png")
+stiny =[]
+stiny.append(wall(180,390,300,30, "img_3.png"))
+
+stiny.append(wall(460,230,20,400, "img_3.png"))
+stiny.append(wall(180,230,20,400, "img_3.png"))
+stiny.append(wall(180,450,20,400, "img_3.png"))
+def defeat():
+    roma.hitbox.x = 320
+    roma.hitbox.y = 260
+    lever.change_photo("Right_lever.png")
+    stiny[0].hitbox.x = 180
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
         if roma.hitbox.colliderect(worog.hitbox):
-            print("ПРОГРАВ!!!!")
-            pygame.quit()
+            defeat()
         if roma.hitbox.colliderect(apple1.hitbox):
-            apple1.x == 21093190
+             apple1.hitbox.x = 2993213
+        for styna in stiny:
+            if roma.hitbox.colliderect(styna.hitbox):
+                defeat()
+        if roma.hitbox.colliderect(worog1.hitbox):
+            defeat()
+        if roma.hitbox.colliderect(lever.hitbox):
+            lever.change_photo("Left_lever.png")
+            sound.play()
+            stiny[0].hitbox.x = 29319319
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_e]:
+            back_theme += 1
+            back.change_photo("img_6.png")
+        if back_theme == 3:
+            back_theme = 0
+            back.change_photo("Space_background.png")
+
+
+
+
+
 
 
     roma.move()
     worog.move()
     worog1.move()
-    worog2.move_y()
-    worog3.move_y()
-    worog4.move_y()
-    window.blit(background, (0, 0))
+
+    back.draw(window)
+    apple1.draw(window)
     roma.draw(window)
     worog.draw(window)
     worog1.draw(window)
-    worog2.draw(window)
-    worog3.draw(window)
-    worog4.draw(window)
-    apple1.draw(window)
-    apple2.draw(window)
-    apple3.draw(window)
-    apple4.draw(window)
+    lever.draw(window)
+    for stina in stiny:
+        stina.draw(window)
     pygame.display.flip()
     fps.tick(60)
 
